cbe389786dada41a9ac681087e1d23176908884e
shahwaizofflical-ux <shahwaizofflical@gmail.com>
2026-07-28 15:55:10 +0500

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,6 @@
+module.exports = {
+    giphyApiKey: process.env.GIPHY_API_KEY || 'dc6zaTOxFJmzC',
+    ownerNumber: process.env.OWNER_NUMBER || '923039507788',
+    botName: '𝗟𝗘𝗚𝗘𝗡𝗗 𝗠𝗗 𝗕𝗢𝗧',
+    ownerName: '𝗟𝗘𝗚𝗘𝗡𝗗 𝗛𝗔𝗦𝗦𝗔𝗡'
+};
